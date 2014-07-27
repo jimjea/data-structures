c8853dfc44d4a54d0622b8cd02768bc4282d6e0a
jimjea <jimjea@gmail.com>
2014-07-27 01:03:39 -0700

    Set Already Takes Non-Strings; Added Tests to Spec for Numbers and Objects

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -11,11 +11,25 @@ describe('set', function() {
     expect(set.remove).to.be.a("function");
   });
 
-  it('should add values to a set', function(){
+  it('should add strings to a set', function(){
     set.add("Susan Sarandon");
     set.add("Danny Glover");
     expect(set.contains('Danny Glover')).to.equal(true);
     expect(set.contains('Susan Sarandon')).to.equal(true);
+  });
+
+  it('should add numbers to a set', function(){
+    set.add(1);
+    set.add(2);
+    expect(set.contains(1)).to.equal(true);
+    expect(set.contains(2)).to.equal(true);
+  });  
+
+  it('should add objects to a set', function(){
+    set.add([1,2,3]);
+    set.add({"key":"value"});
+    expect(set.contains([1,2,3])).to.equal(true);
+    expect(set.contains({"key":"value"})).to.equal(true);
   });
 
   it('should remove values from a set', function(){
